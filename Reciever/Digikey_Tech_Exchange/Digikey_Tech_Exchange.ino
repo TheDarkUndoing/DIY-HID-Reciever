5de5b165447d89399af12d5c7f41552326507623
TheDarkUndoing <38772798+TheDarkUndoing@users.noreply.github.com>
2019-11-01 12:56:18 -0400

    Tech exchange modification

--- Reciever/Digikey_Tech_Exchange/Digikey_Tech_Exchange.ino
+++ Reciever/Digikey_Tech_Exchange/Digikey_Tech_Exchange.ino
@@ -1,12 +1,11 @@
 
 #include "DigiKeyboard.h"
 void setup() {
-//delay(5000);
+
 DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT);
-DigiKeyboard.print("powershell");
-delay(1000);
+DigiKeyboard.println("powershell");
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
-delay(1000);
+delay(5000);
 DigiKeyboard.println("echo \"I now have full access to the command prompt\"");
 
 
